420bc6f0e8ca95832062e58921df9ae26b970251
Stefan Jenkner <stefan@jenkner.org>
2019-03-17 19:17:20 +0100

    Displaying images read as .dat files from SPIFFS.

--- vsmp/vsmp.ino
+++ vsmp/vsmp.ino
@@ -1,50 +1,88 @@
 
 #define ENABLE_GxEPD2_GFX 0
 
+#include <FS.h>
 #include <GxEPD2_BW.h>
 
 // ESP8266 ePaper board
 GxEPD2_BW < GxEPD2_750, GxEPD2_750::HEIGHT / 2 > display(GxEPD2_750(/*CS=15*/ SS, /*DC=4*/ 4, /*RST=5*/ 5, /*BUSY=16*/ 16));
 
-#include "frames.h"
+// initial image in SPIFFS
+String imageName = String("000.dat");
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("setup");
-  display.init(115200);
+  SPIFFS.begin();
 
-  display.firstPage();
-  display.fillScreen(GxEPD_WHITE);
-  display.nextPage();
+  Serial.begin(115200);
+  //while(!Serial) {
+  //  ; // wait
+  //}
   delay(2000);
 
-  const unsigned char* bitmaps[] = { myBitmap_1, myBitmap_2, myBitmap_3, myBitmap_4, myBitmap_5, myBitmap_6 };
-
-  for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-  {
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawInvertedBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-    }
-    while (display.nextPage());
-    delay(60000);
-  }
-
-  display.firstPage();
-  display.fillScreen(GxEPD_WHITE);
-  while (display.nextPage());
-
-  Serial.println("good bye");
-
-  while (display.nextPage());
-  display.powerOff();
-
+  Serial.println("Init display");
+  display.init(115200);
 }
+
+void displayImage(String filename, uint16_t width, uint16_t height) {
+
+  fs::File file;
+  Serial.print(String("Loading image: ") + filename);
+  file = SPIFFS.open(String("/") + filename, "r");
+  if (!file) {
+    Serial.print("File not found");
+    return;
+  }
+  uint8_t data[width];
+  for (uint16_t y = 0; y < height ; y += 8) {
+    file.read(data, width /* multiply by 8 rows divide by 8 bits */ );
+    // void writeImage(const uint8_t bitmap[], int16_t x, int16_t y, int16_t w, int16_t h, bool invert = false, bool mirror_y = false, bool pgm = false);
+    display.writeImage(data, 0, y, width, 8);
+  }
+  display.refresh();
+  file.close();
+}
+
+String findNextFile(String prevFile) {
+
+  String nextFile = String("");
+  fs::Dir dir = SPIFFS.openDir("");
+
+  while (dir.next()) {
+    String fileName = dir.fileName();
+    fileName = fileName.substring(1); // strip '/'
+
+    // skip non-supported files
+    if (!fileName.endsWith(".dat")) {
+      continue;
+    }
+
+    if (prevFile.compareTo(fileName) < 0) {
+      // first valid match OR found better match
+      if ((nextFile == "") || (fileName.compareTo(nextFile) < 0)) {
+        nextFile = fileName;
+      }
+    }
+  }
+  return nextFile;
+}
+
 
 void loop()
 {
+  imageName = findNextFile(imageName);
+
+  // nothing to show?
+  if (imageName == "") {
+    display.firstPage();
+    display.fillScreen(GxEPD_WHITE);
+    display.nextPage();
+    display.powerOff();
+    Serial.println("Good night");
+    ESP.restart();
+  }
+
+  displayImage(imageName, 640, 384);
+  delay(60000);
+
 }
